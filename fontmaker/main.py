caeade6c3094fa467e9c6ae89df4c5149db04b8e
Rodrigo Delduca <rodrigo@delduca.org>
2025-04-13 08:22:59 -0300

    Work in progress

--- fontmaker/main.py
+++ fontmaker/main.py
@@ -1,29 +1,16 @@
 import argparse
 import json
+import re
 import os
 
 import freetype
 from PIL import Image, ImageDraw, ImageFont
 
-
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-d",
-        "--directory",
-        required=True,
-    )
-    parser.add_argument(
-        "-f",
-        "--font",
-        required=True,
-    )
-    parser.add_argument(
-        "-s",
-        "--size",
-        type=int,
-        required=True,
-    )
+    parser.add_argument("-d", "--directory", required=True)
+    parser.add_argument("-f", "--font", required=True)
+    parser.add_argument("-s", "--size", type=int, required=True)
     arguments = parser.parse_args()
 
     face = freetype.Face(arguments.font)
@@ -48,12 +35,24 @@ def main():
         h = bbox[3] - bbox[1]
         offset_x = (cell_width - w) // 2 - bbox[0]
         offset_y = (cell_height - h) // 2 - bbox[1]
-        draw.text((x + offset_x, offset_y), ch, font=font, fill=(255, 255, 255, 255))
+
+        scale = 4
+        hires_size = (cell_width * scale, cell_height * scale)
+        glyph_img = Image.new("L", hires_size, 0)
+        hires_font = ImageFont.truetype(arguments.font, arguments.size * scale)
+        glyph_draw = ImageDraw.Draw(glyph_img)
+        glyph_draw.text((offset_x * scale, offset_y * scale), ch, font=hires_font, fill=255)
+
+        glyph_img = glyph_img.resize((cell_width, cell_height), resample=Image.LANCZOS)
+        rgba_glyph = Image.new("RGBA", (cell_width, cell_height), (255, 255, 255, 0))
+        rgba_glyph.putalpha(glyph_img)
+        sheet.alpha_composite(rgba_glyph, dest=(x, 0))
+
         x += cell_width
         draw.line([(x, 0), (x, cell_height)], fill=(255, 0, 255, 255), width=1)
         x += 1
 
-    name = os.path.splitext(os.path.basename(arguments.font))[0]
+    name = re.sub(r"[^a-z0-9]", "", os.path.splitext(os.path.basename(arguments.font).lower())[0])
     blob_dir = os.path.join(arguments.directory, "blobs")
     font_dir = os.path.join(arguments.directory, "fonts")
 
@@ -71,12 +70,12 @@ def main():
                 "glyphs": "".join(glyphs),
                 "spritesheet": f"blobs/{name}.png",
                 "spacing": 0,
+                "leading": 0,
             },
             f,
             indent=2,
             ensure_ascii=False,
         )
-
 
 if __name__ == "__main__":
     main()
